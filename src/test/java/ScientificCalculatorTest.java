dc369940822809ab73d19b386a98b08897416116
Palak Goyal <goyal.palak01@gmail.com>
2021-03-11 17:51:36 +0530

    Update test class with true/false,positive/negative test cases

--- src/test/java/ScientificCalculatorTest.java
+++ src/test/java/ScientificCalculatorTest.java
@@ -6,71 +6,112 @@ public class ScientificCalculatorTest {
 
     private static final double DELTA = 1e-15;
     ScientificCalculator sc = new ScientificCalculator();
-
+    //factorial test
     @Test
-    public  void  factorialPositiveTest()
+    public  void  factorialTruePositiveTest()
     {
-        assertEquals("{factorialPositiveTest1}",24.0,sc.calculateFactorial(4),DELTA);
-        assertEquals("{factorialPositiveTest2}",Double.NaN,sc.calculateFactorial(-20),DELTA);
+        assertEquals("{factorialTruePositiveTest1}",24.0,sc.calculateFactorial(4),DELTA);
+        assertEquals("{factorialTruePositiveTest2}",1.0,sc.calculateFactorial(0),DELTA);
     }
-
     @Test
-    public void factorialNegativeTest()
+    public  void  factorialTrueNegativeTest()
     {
-        assertNotEquals("{factorialNegativeTest1}",42.0,sc.calculateFactorial(4),DELTA);
-        assertNotEquals("{factorialNegativeTest2}",6.0,sc.calculateFactorial(-20),DELTA);
+        assertEquals("{factorialTrueNegativeTest1}",Double.NaN,sc.calculateFactorial(-20),DELTA);
     }
-
     @Test
-    public void exponentiationPositiveTest()
+    public  void  factorialFalsePositiveTest()
     {
-        assertEquals("{exponentPositiveTest1}",Double.NaN,sc.calculateExp(0,0),DELTA);
-        assertEquals("{exponentPositiveTest2}",81.0,sc.calculateExp(3,4),DELTA);
-        assertEquals("{exponentPositiveTest3}",0.125,sc.calculateExp(2,-3),DELTA);
-
+        assertNotEquals("{factorialFalsePositiveTest1}",42.0,sc.calculateFactorial(4),DELTA);
+        assertNotEquals("{factorialFalsePositiveTest2}",11.0,sc.calculateFactorial(0),DELTA);
     }
-
-
     @Test
-    public void exponentiationNegativeTest()
+    public  void  factorialFalseNegativeTest()
     {
-        assertNotEquals("{exponentNegativeTest3}",1,sc.calculateExp(0,0),DELTA);
-        assertNotEquals("{exponentNegativeTest1}",32,sc.calculateExp(3,4),DELTA) ;
-        assertNotEquals("{exponentNegativeTest2}",0.125,sc.calculateExp(3,-4),DELTA);
+        assertNotEquals("{factorialFalseNegativeTest1}",24.0,sc.calculateFactorial(-20),DELTA);
 
     }
 
+    //exponentiation test
     @Test
-    public void logarithmPositiveTest()
+    public void exponentiationTruePositiveTest()
     {
-        assertEquals("{logarithmPositiveTest1}",0.6931471805599453,sc.calculateLog(2),DELTA);
-        assertEquals("{logarithmPositiveTest2}",Double.NaN,sc.calculateLog(0),DELTA);
-        assertEquals("{logarithmPositiveTest3}, ",Double.NaN,sc.calculateLog(-8),DELTA);
+        assertEquals("{exponentiationTruePositiveTest1}",81.0,sc.calculateExp(3,4),DELTA);
+        assertEquals("{exponentiationTruePositiveTest2}",0.125,sc.calculateExp(2,-3),DELTA);
+        assertEquals("{exponentiationTruePositiveTest3}",0.25,sc.calculateExp(-2,-2),DELTA);
     }
-
-
     @Test
-    public void logarithmNegativeTest()
+    public void exponentiationTrueNegativeTest()
     {
-        assertNotEquals("{logarithmNegativeTest1}", 2.2, sc.calculateLog(3), DELTA);
-        assertNotEquals("{logarithmNegativeTest2}", 0, sc.calculateLog(0), DELTA);
-        assertNotEquals("{logarithmNegativeTest3}", 4.0, sc.calculateLog(-5.3), DELTA);
+        assertEquals("{exponentiationTrueNegativeTest1}",Double.NaN,sc.calculateExp(0,0),DELTA);
+
+    }
+    @Test
+    public void exponentiationFalsePositiveTest()
+    {
+        assertNotEquals("{exponentiationFalsePositiveTest1}",91.0,sc.calculateExp(3,4),DELTA);
+        assertNotEquals("{exponentiationFalsePositiveTest2}",125.0,sc.calculateExp(2,-3),DELTA);
+        assertNotEquals("{exponentiationFalsePositiveTest3}",Double.NaN,sc.calculateExp(-2,-2),DELTA);
+    }
+    @Test
+    public void exponentiationFalseNegativeTest()
+    {
+        assertNotEquals("{exponentiationFalseNegativeTest1}",1.0,sc.calculateExp(0,0),DELTA);
+
     }
 
+    //logarithm test
+    @Test
+    public void logarithmTruePositiveTest()
+    {
+        assertEquals("{logarithmTruePositiveTest1}",0.6931471805599453,sc.calculateLog(2),DELTA);
+        assertEquals("{logarithmTruePositiveTest2}",1.667706820558076,sc.calculateLog(5.3),DELTA);
+    }
+    @Test
+    public void logarithmTrueNegativeTest()
+    {
+        assertEquals("{logarithmTrueNegativeTest1}",Double.NaN,sc.calculateLog(0),DELTA);
+        assertEquals("{logarithmTrueNegativeTest2}",Double.NaN,sc.calculateLog(-4),DELTA);
+    }
+    @Test
+    public void logarithmFalsePositiveTest()
+    {
+        assertNotEquals("{logarithmFalsePositiveTest1}",6.9,sc.calculateLog(2),DELTA);
+
+    }
+    @Test
+    public void logarithmFalseNegativeTest()
+    {
+        assertNotEquals("{logarithmFalseNegativeTest1}",1.0,sc.calculateLog(0),DELTA);
+        assertNotEquals("{logarithmFalseNegativeTest2}",4.0,sc.calculateLog(-5.3),DELTA);
+    }
+
+    //square root test
 
     @Test
-    public void sqrtPositiveTest(){
-        assertEquals("{sqrtPositiveTest1}", 20.0, sc.calculateSqrt(400), DELTA);
-        assertEquals("{sqrtPositiveTest2}", 2.6457513110645907, sc.calculateSqrt(7), DELTA);
-        assertEquals("{sqrtPositiveTest3}", 0.0, sc.calculateSqrt(0), DELTA);
-        assertEquals("{sqrtPositiveTest4}", Double.NaN, sc.calculateSqrt(-5), DELTA);
+    public void sqrtTruePositiveTest(){
+
+        assertEquals("{sqrtTruePositiveTest1}", 2.6457513110645907, sc.calculateSqrt(7), DELTA);
+        assertEquals("{sqrtTruePositiveTest2}", 0.0, sc.calculateSqrt(0), DELTA);
+        assertEquals("{sqrtTruePositiveTest3}", 1.224744871391589, sc.calculateSqrt(1.5), DELTA);
+    }
+    @Test
+    public void sqrtTrueNegativeTest(){
+
+        assertEquals("{sqrtTrueNegativeTest1}", Double.NaN, sc.calculateSqrt(-7), DELTA);
+
+    }
+    @Test
+    public void sqrtFalsePositiveTest(){
+
+        assertNotEquals("{sqrtFalsePositiveTest1}", 2.0, sc.calculateSqrt(7), DELTA);
+        assertNotEquals("{sqrtFalsePositiveTest2}",Double.NaN, sc.calculateSqrt(0), DELTA);
+        assertNotEquals("{sqrtFalsePositiveTest3}", 1.0, sc.calculateSqrt(1.5), DELTA);
     }
 
     @Test
-    public void sqrtNegativeTest(){
-        assertNotEquals("{sqrtNegativeTest1}", 6.9, sc.calculateSqrt(400), DELTA);
-        assertNotEquals("{sqrtNegativeTest2}", 9.0, sc.calculateSqrt(3.4), DELTA);
-        assertNotEquals("{sqrtNegativeTest3}", 3.0, sc.calculateSqrt(-9), DELTA);
+    public void sqrtFalseNegativeTest(){
+
+        assertNotEquals("{sqrtNegativeTest3}", -3.0, sc.calculateSqrt(-9), DELTA);
     }
 
 }
